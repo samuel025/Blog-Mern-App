1d22ea5e8faddb364db284f96f7ff5afab756e39
Olayemi Samuel <sam_olayemi65@yahoo.com>
2024-02-16 13:39:02 +0100

    deleted uploads

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -7,12 +7,12 @@ const getBlogs = async_handler(async (req, res) => {
 })
 
 const createBlog = async_handler(async (req, res) => {
-    if(!req.body.title || !req.body.description || !req.file) {
+    if(!req.body.title || !req.body.description || !req.image) {
         res.status(400)
         throw new Error('Please fill all required fields')
     }
     const {title, description} = req.body
-    const newBlog = new Blog({title, description, image : req.file})
+    const newBlog = new Blog({title, description, image_url : req.image})
     await newBlog.save()
     res.status(200).json(newBlog)
 })
